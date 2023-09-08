06d2ccceef5466a6ceda3161dabf1d3275652296
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2023-09-08 09:09:04 +0700

    feat: add comments

--- Callback-Style.c
+++ Callback-Style.c
@@ -5,52 +5,53 @@
 #include "esp_log.h"
 #include "driver/gpio.h"
 #include "rom/gpio.h"
-// Define variables to store timestamps
-uint64_t isr_start_time;
-uint64_t isr_end_time;
 
-uint64_t expected_interval = 1000000; // 1 second in microseconds
-uint64_t prev_interrupt_time = 0;
+// Deklarasi variabel untuk menyimpan waktu mulai dan selesai
+uint64_t start_time;
+uint64_t end_time;
 
 void timer_interrupt(void *param)
 {
     static bool ON;
     ON = !ON;
 
-    // Capture timestamp at the beginning of the ISR
-    isr_start_time = esp_timer_get_time();
+    // Mendapatkan waktu mulai interrupt
+    start_time = esp_timer_get_time();
 
     for (int i = 0; i < 1000000; i++)
     {
-        // Intentional delay
+        // Delay untuk memperlambat interrupt, supaya latency lebih terlihat
     }
 
     gpio_set_level(GPIO_NUM_2, ON);
 
-    // Capture timestamp at the end of the ISR
-    isr_end_time = esp_timer_get_time();
+    // Mendapatkan waktu selesai interrupt
+    end_time = esp_timer_get_time();
 }
 
 void app_main(void)
 {
+    // Konfigurasi GPIO
     gpio_pad_select_gpio(GPIO_NUM_2);
     gpio_set_direction(GPIO_NUM_2, GPIO_MODE_OUTPUT);
 
+    // Konfigurasi timer
     const esp_timer_create_args_t my_timer_args = {
         .callback = &timer_interrupt,
         .name = "My Timer"};
 
+    // Membuat timer
     esp_timer_handle_t timer_handler;
 
+    // Memulai timer
     ESP_ERROR_CHECK(esp_timer_create(&my_timer_args, &timer_handler));
     ESP_ERROR_CHECK(esp_timer_start_periodic(timer_handler, 1000000)); // 1000000 us = 1 s
 
     while (true)
     {
-        // Calculate and print interrupt latency
-        uint64_t interrupt_latency_us = isr_end_time - isr_start_time;
+        // Menampilkan latency interrupt
+        uint64_t interrupt_latency_us = end_time - start_time;
         printf("Interrupt Latency - Callback: %llu microseconds\n", interrupt_latency_us);
-
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
--- ISR-Style.c
+++ ISR-Style.c
@@ -5,30 +5,32 @@
 #include "esp_timer.h"
 #include "rom/gpio.h"
 
-// Define variables to store timestamps
+// Deklarasi variabel untuk menyimpan waktu mulai dan selesai
 uint64_t timer_start_time;
 uint64_t timer_end_time;
 
-// Function pointer for the IRAM routine
+// Fungsi yang akan dipanggil ketika timer interrupt terjadi
 void IRAM_ATTR timer_callback(void *arg)
 {
   static bool ON;
   ON = !ON;
 
-  // Capture timestamp at the beginning of the timer callback
+  // Mendapatkan waktu mulai interrupt
   timer_start_time = esp_timer_get_time();
 
   for (int i = 0; i < 1000000; i++)
   {
-    // Intentional delay
+    // Delay untuk memperlambat interrupt, supaya latency lebih terlihat
   }
 
-  // Toggle the LED state
+  // Set GPIO 2 ke nilai ON
   gpio_set_level(GPIO_NUM_2, ON);
 
-  // Capture timestamp at the end of the timer callback
+  // Mendapatkan waktu selesai interrupt
   timer_end_time = esp_timer_get_time();
   uint64_t interrupt_latency = timer_end_time - timer_start_time;
+
+  // Menampilkan latency interrupt
   printf("Interrupt Latency - ISR: %llu microseconds\n", interrupt_latency);
 }
 
@@ -38,15 +40,15 @@ void app_main(void)
   gpio_set_direction(GPIO_NUM_2, GPIO_MODE_OUTPUT);
   gpio_set_level(GPIO_NUM_2, 0);
 
-  // Define the timer configuration
+  // Konfigurasi timer
   esp_timer_create_args_t timer_config = {
       .callback = &timer_callback,
       .name = "my_timer"};
 
-  // Create and start the timer
+  // Membuat timer
   esp_timer_handle_t timer;
   esp_timer_create(&timer_config, &timer);
-  esp_timer_start_periodic(timer, 1000000); // 1 second interval
+  esp_timer_start_periodic(timer, 1000000); // interval 1 s
 
   while (1)
   {
